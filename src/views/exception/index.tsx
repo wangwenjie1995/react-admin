8ae19143cf53cc6bac28dc7abba698407912c2c9
wagnwenjie <wagnwenjie1995@gmail.com>
2024-12-08 00:39:28 +0800

    test:测试

--- src/views/exception/index.tsx
+++ src/views/exception/index.tsx
@@ -6,7 +6,7 @@ import { PageWrapper } from '@/components/Page'
 import { RESULT_COMPO } from '@/settings/websiteSetting'
 
 const subTitleMap = new Map([
-  [ExceptionEnum.PAGE_NOT_ACCESS, '对不起，您没有权限访问此页面。'],
+  [ExceptionEnum.PAGE_NOT_ACCESS, '对不起1，您没有权限访问此页面。'],
   [ExceptionEnum.PAGE_NOT_FOUND, '对不起，您访问的页面不存在。'],
   [ExceptionEnum.SERVER_ERROR, '对不起，服务器发生错误。']
 ])
