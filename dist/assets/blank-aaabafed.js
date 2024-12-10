39819288b49a55e9964fe749e100e8d66ec56deb
wagnwenjie <wagnwenjie1995@gmail.com>
2024-12-10 18:23:09 +0800

    feature: 放开ignore线下编译,阿里云编译老是卡死

---
+++ dist/assets/blank-aaabafed.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2823d128.js";const t=()=>e.jsx("div",{className:"flex-center",style:{width:"100%",height:"500px",fontSize:"32px"},children:"页面开发中。。。"});export{t as default};
